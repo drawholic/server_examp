42754378629a87620937ec32a1d070f2d629cf3f
drawholic <mamrokhavlad@gmail.com>
2025-03-09 13:08:55 +0200

    get_message declare, removed global msg

--- include/Client.hpp
+++ include/Client.hpp
@@ -4,8 +4,11 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
 
-const char* message = "Hello server!";
+// const char* message = "Hello server!";
 const unsigned BUFFER_SIZE = 1024;
 
 class Client
@@ -15,12 +18,13 @@ class Client
 	sockaddr_in addr;
 	socklen_t addr_len;
 	char* buffer;
+	const char* message;
 
 	void create_socket();
 	void fill_address(const char*, int);
 	void connect_to_server();
 	void send_message();
-
+	void get_message();
 public:
 	Client(const char* ip = 0, int port = 0);
 	~Client();
