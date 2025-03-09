8e7a7b3ba20467ccdb2fa7e6232a96c54f3c9831
drawholic <mamrokhavlad@gmail.com>
2025-03-09 16:47:10 +0200

    consts BUFFER_SIZE and DEFAULT_PORT, client desc, set_options method added, updated default constructor parameters and fill_address parameters added

--- include/Server.hpp
+++ include/Server.hpp
@@ -8,18 +8,21 @@
 #include <cstdlib>
 #include <unistd.h>
 
+const unsigned BUFFER_SIZE = 1024;
+const unsigned DEFAULT_PORT = 8000;
 class Server
 {
 	int status;
-	int fd;
+	int fd, client;
 	
 	sockaddr_in addr;
 	socklen_t addr_len;
 
 	char* buffer;
 
+	void set_options();
 	void create_socket();
-	void fill_address();
+	void fill_address(const char*, int);
 	void socket_listen();
 	void bind_addr();
 	void server_accept();
@@ -27,7 +30,7 @@ class Server
 	void send_message();
 
 public:
-	Server(const char* ip = 0, int port);
+	Server(const char* ip = 0, int port = 0);
 	~Server();
 
 };
