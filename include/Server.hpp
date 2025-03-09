a977be1e4b5d32302f8c99f63c7f998baf72e392
drawholic <mamrokhavlad@gmail.com>
2025-03-09 13:13:36 +0200

    class declaration

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,8 +1,29 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+
+
 class Server
 {
+	int status;
+	int fd;
+	
+	sockaddr_in addr;
+	socklen_t addr_len;
+
+	char* buffer;
+
+	void create_socket();
+	void fill_address();
+	void socket_listen();
+	void bind_addr();
+	void server_accept();
+	void get_message();
+	void send_message();
+
+public:
+	Server(const char* ip = 0, int port);
+	~Server();
 
 };
 
