a5044be3f875584ea57915f008a9cc7f208e8521
drawholic <mamrokhavlad@gmail.com>
2025-03-09 12:52:45 +0200

    class declaration

---
+++ include/Client.hpp
@@ -0,0 +1,31 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+const char* message = "Hello server!";
+const unsigned BUFFER_SIZE = 1024;
+
+class Client
+{
+	int fd;
+	sockaddr_in addr;
+	socklen_t addr_len;
+	char* buffer;
+
+	void create_socket();
+	void fill_address(const char*, int);
+	void connect_to_server();
+	void send_message();
+
+public:
+	Client(const char* ip = 0, int port = 0);
+	~Client();
+
+	void run();
+};
+
+
+#endif
