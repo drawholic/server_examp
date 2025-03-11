4aa7dc34e404814e5cffc237a7d81c1e8e685e49
drawholic <mamrokhavlad@gmail.com>
2025-03-11 16:53:34 +0200

    restructure main file

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,12 +1,28 @@
 #include <cstdio>
 #include "Server.hpp"
+#include <csignal>
+
+
+Server* s;
+
+void handle_interrupt(int);
 
 int main(int argc, char const *argv[])
 {
 	const char* ip = "0.0.0.0";
 	int port = 8000;
-	Server s(ip, port);
 
+	s = new Server(ip, port);
+
+	s->run();
 	printf("Hello world!\n");
 	return 0;
 }
+
+
+void handle_interrupt(int sig)
+{
+	printf("Interrupting...\n");
+	s->stop();
+};
+
