ba4085adff1fe5b7d03ab8e5f0ee5b5e97504d93
drawholic <mamrokhavlad@gmail.com>
2025-03-11 15:16:18 +0200

    Server init call

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,8 +1,12 @@
 #include <cstdio>
-
+#include "Server.hpp"
 
 int main(int argc, char const *argv[])
 {
+	const char* ip = "0.0.0.0";
+	int port = 8000;
+	Server s(ip, port);
+
 	printf("Hello world!\n");
 	return 0;
 }
